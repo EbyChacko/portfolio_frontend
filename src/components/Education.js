7b5338d22ec37ad04c9aa9043ce02d757ba07fc5
Eby <eby@MAC-915C57.station>
2024-10-23 21:40:58 +0100

    add css to education-card

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,8 +1,8 @@
 import { Container, Row, Col } from "react-bootstrap";
 import { EducationCard } from "./EducationCard";
-// import code_certificate from '../assets/img/certificates/code_certificate.png';
-// import cadd_certificate from '../assets/img/certificates/cadd_certificate.jpg';
-// import bca_certificate from '../assets/img/certificates/bca_certificate.jpg';
+import code_certificate from '../assets/img/certificates/code_certificate.png';
+import cadd_certificate from '../assets/img/certificates/cadd_certificate.jpg';
+import bca_certificate from '../assets/img/certificates/bca_certificate.jpg';
 
 export const Education = () => {
   const educations = [
@@ -11,21 +11,21 @@ export const Education = () => {
             duration: "May 2023 - May 2024",
             institute: 'Code Institute',
             grade: 'Distinction',
-            // cert_link : code_certificate,
+            cert_link : code_certificate,
         },
         {
             cource:"Bachelor of Computer Application",
             duration: "Mar 2012 - Mar 2015",
             institute: 'Ilahia Collage of Arts & Science',
             grade: 'B',
-            // cert_link:bca_certificate,
+            cert_link:bca_certificate,
         },
         {
             cource:"Master Diploma in Animation",
             duration: "Oct 2007 - Apr 2009",
             institute: 'Cochin Cadd Center',
             grade: 'B',
-            // cert_link: cadd_certificate,
+            cert_link: cadd_certificate,
         },
     ];
 
--- src/components/EducationCard.js
+++ src/components/EducationCard.js
@@ -2,17 +2,17 @@ import { Col } from "react-bootstrap";
 
 export const EducationCard = ({ cource, duration, institute, grade, cert_link }) => {
   return (
-    <Col size={12} md={6} xl={4} className="Education-card">
-        <div className="proj-imgbx">
-            <div className="proj-txtx">
+    <Col size={12} md={6} className="Education-card">
+        <div className="">
+            <div className="">
             <h4>{cource}</h4>
             <span>{duration}</span>
             <h4>{institute}</h4>
             <h5>{grade}</h5>
             <br></br>
-            {/* <a href={cert_link} target="_blank" rel="noopener noreferrer">
+            <a href={cert_link} target="_blank" rel="noopener noreferrer">
             <button className="live-btn">View Certificate</button>
-            </a> */}
+            </a>
             </div>
         </div>
 
