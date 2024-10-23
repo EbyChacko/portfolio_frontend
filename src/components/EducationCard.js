5a6a618b8779ea4d1e4b99f92c0e5cd648c04e35
Eby <eby@MAC-915C57.station>
2024-10-23 21:56:04 +0100

    change the css for education card

--- src/components/EducationCard.js
+++ src/components/EducationCard.js
@@ -4,14 +4,11 @@ export const EducationCard = ({ cource, duration, institute, grade }) => {
   return (
     <Col size={12} md={6} className="Education-card">
         <div className="">
-            <div className="">
-            <h4>{cource}</h4>
-            <span>{duration}</span>
-            <h4>{institute}</h4>
-            <h5>{grade}</h5>
-            </div>
+          <h4>{cource}</h4>
+          <span>{duration}</span>
+          <h5>{institute}</h5>
+          <p>Grade : {grade}</p>
         </div>
-
     </Col>
   )
 }
