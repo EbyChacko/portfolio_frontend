a94ba6ce39a14024701cb50612167b3f21959183
Eby <eby@MAC-915C57.station>
2024-10-23 19:15:15 +0100

    add educationCard

--- frontend/src/components/Education.js
+++ frontend/src/components/Education.js
@@ -1,32 +1,57 @@
 import { Container, Row, Col } from "react-bootstrap";
+import { EducationCard } from "./EducationCard";
+import code_certificate from '../assets/img/Certificate/code_certificate.jpg';
+import cadd_certificate from '../assets/img/Certificate/cadd_certificate.jpg';
+import bca_certificate from '../assets/img/Certificate/bca_certificate.jpg';
 
 export const Education = () => {
-  const linkedin = "https://www.linkedin.com/in/eby-chacko-85695b245/";
-  const github = "https://github.com/EbyChacko";
+  const educations = [
+        {
+            cource:"Diploma in Full Stack Development",
+            duration: "May 2023 - May 2024",
+            institute: 'Code Institute',
+            grade: 'Distinction',
+            cert_link : code_certificate,
+        },
+        {
+            cource:"Bachelor of Computer Application",
+            duration: "Mar 2012 - Mar 2015",
+            institute: 'Ilahia Collage of Arts & Science',
+            grade: 'B',
+            cert_link:bca_certificate,
+        },
+        {
+            cource:"Master Diploma in Animation",
+            duration: "Oct 2007 - Apr 2009",
+            institute: 'Cochin Cadd Center',
+            grade: 'B',
+            cert_link: cadd_certificate,
+        },
+    ];
 
   return (
     <section className="education" id="education">
       <Container>
-        <Row className="">
-          <Col size={12}>
-            <div>
-              <h2>EDUCATION</h2>
-
-              <Row className="g-3">
-                  <Col size={12} sm={6} className="edu_box">
-                    <h4>Deploma in Full stack development</h4>
-                    <span>May 2023 - May 2024</span>
-                    <h5>Code Institute</h5>
-                  </Col>
-
-                  <Col size={12} sm={6} className="edu_box">
-
-                  </Col>
+                <Row>
+                    <Col size={12}>
+                        <div >
+                            <h2>EDUCATION</h2>
+                            <Row className="row justify-content-center">
+                                {
+                                educations.map((education, index) => {
+                                    return (
+                                    <EducationCard
+                                        key={index}
+                                        {...education}
+                                        />
+                                    )
+                                })
+                                }
+                            </Row>
+                        </div>
+                    </Col>
                 </Row>
-            </div>
-          </Col>
-        </Row>
-      </Container>
+            </Container>
     </section>
   );
 };
---
+++ frontend/src/components/EducationCard.js
@@ -0,0 +1,21 @@
+import { Col } from "react-bootstrap";
+
+export const EducationCard = ({ cource, duration, institute, grade,cert_link }) => {
+  return (
+    <Col size={12} md={6} xl={4} className="Education-card">
+        <div className="proj-imgbx">
+            <div className="proj-txtx">
+            <h4>{cource}</h4>
+            <span>{duration}</span>
+            <h4>{institute}</h4>
+            <h5>{grade}</h5>
+            <br></br>
+            <a href={cert_link} target="_blank" rel="noopener noreferrer">
+            <button className="live-btn">View Certificate</button>
+            </a>
+            </div>
+        </div>
+
+    </Col>
+  )
+}
