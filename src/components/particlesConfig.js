bac5ac04760551a7f4e423d63cb2a75b5c29a32d
Eby <eby@MAC-915C57.station>
2024-10-24 20:45:24 +0100

    add particle effect

---
+++ src/components/particlesConfig.js
@@ -0,0 +1,64 @@
+const particlesConfig = {
+    background: {
+      color: {
+        value: "#000000", // Background color
+      },
+    },
+    fpsLimit: 60,
+    interactivity: {
+      events: {
+        onClick: {
+          enable: true,
+          mode: "push",
+        },
+        onHover: {
+          enable: true,
+          mode: "repulse",
+        },
+        resize: true,
+      },
+    },
+    particles: {
+      color: {
+        value: "#ffffff", // Particle color
+      },
+      links: {
+        color: "#ffffff",
+        distance: 150,
+        enable: true,
+        opacity: 0.5,
+        width: 1,
+      },
+      collisions: {
+        enable: true,
+      },
+      move: {
+        direction: "none",
+        enable: true,
+        outMode: "bounce",
+        random: false,
+        speed: 3,
+        straight: false,
+      },
+      number: {
+        density: {
+          enable: true,
+          area: 800,
+        },
+        value: 80, // Number of particles
+      },
+      opacity: {
+        value: 0.5,
+      },
+      shape: {
+        type: "circle",
+      },
+      size: {
+        random: true,
+        value: 5,
+      },
+    },
+    detectRetina: true,
+  };
+  
+  export default particlesConfig;
