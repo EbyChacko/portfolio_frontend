121ffbdb978ae5f943a2b10d59b648ecd3ef758b
Eby <eby@MAC-915C57.station>
2024-10-24 16:33:40 +0100

    add css to the experience-card

--- src/components/ExperienceCard.js
+++ src/components/ExperienceCard.js
@@ -2,7 +2,7 @@ import { Col } from "react-bootstrap";
 
 export const ExperienceCard = ({ role, duration, company, place }) => {
   return (
-    <Col size={12} className="Education-card">
+    <Col size={12} sm={12} className="Education-card">
         <div className="">
           <h4>{role}</h4>
           <p>{duration}</p>
