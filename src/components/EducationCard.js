d8b1558590b0dbacb438773ca914f8c7ecd664bb
Eby <eby@MAC-915C57.station>
2024-10-24 09:13:27 +0100

    add css

--- src/components/EducationCard.js
+++ src/components/EducationCard.js
@@ -2,7 +2,7 @@ import { Col } from "react-bootstrap";
 
 export const EducationCard = ({ cource, duration, institute, grade }) => {
   return (
-    <Col size={12} xl={6} className="Education-card">
+    <Col size={12} xl={5} className="Education-card">
         <div className="">
           <h4>{cource}</h4>
           <span>{duration}</span>
